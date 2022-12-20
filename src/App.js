61ecbb3cec48505d5735df1cd00a77b30736af5e
Teddy1404 <aryansinghchandel1404@gmail.com>
2022-12-20 22:21:37 +0530

    color mode

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import Textarea from './components/Textarea';
+import About from './components/About';
 
 function App() {
   return (
     <>
    <Navbar title ="Aryan" />
    <div className='container'>
-  <Textarea heading="Textarea"/>
+  {/* <Textarea heading="Textarea"/> */}
    </div>
+   <About/>
    </>
   );
 }
