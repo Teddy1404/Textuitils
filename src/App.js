08b54b4a4ac09c7a37346a2acd054876d4e22e5d
Teddy1404 <aryansinghchandel1404@gmail.com>
2022-12-25 22:19:43 +0530

    alert added

--- src/App.js
+++ src/App.js
@@ -7,21 +7,34 @@ import React,{useState} from 'react';
 import Alert from './components/Alert';
 
 function App() {
+  const[alert,setalert]=useState(null);
+  const showalert=(messeage,type)=>{
+setalert({
+  msg :messeage,
+type : type
+})
+
+setTimeout(() => {
+  setalert(null);
+}, 1500);
+  }
 const[mode,setmode] = useState('light');
 const Togglemode = ()=>{
   if(mode==='light'){
     setmode('dark');
     document.body.style.backgroundColor = '#2B547E' ;
+    showalert("Dark mode is enabled","success")
   }
   else{
     setmode('light');
     document.body.style.backgroundColor = 'white' ;
+    showalert("light mode is enabled","success")
   }
 }
   return (
     <>
    <Navbar title ="Aryan" mode={mode} Togglemode={Togglemode}/>
-   <Alert alert="this is alert"/>
+   <Alert alert={alert}/>
    <div className='container'>
   <Textarea heading="Textarea"mode={mode} Togglemode={Togglemode}/>
    </div>
--- src/components/Alert.js
+++ src/components/Alert.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 function Alert(props) {
   return (
-    <div>
-      <div class="alert alert-warning alert-dismissible fade show" role="alert">
-  {props.alert}
-  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    
+   props.alert &&   <div className={`alert alert-${props.alert.type} alert-dismissible fade show `} role="alert">
+  <strong>{props.alert.type}: </strong>{props.alert.msg}
+  
 </div>
-    </div>
+
   )
 }
 
